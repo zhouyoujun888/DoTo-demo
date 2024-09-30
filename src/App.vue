4e5ee95d6fd5a7980bcdc6d46150dfc1afbb539e
周宥均 <a390687273@qq.com>
2024-09-30 16:24:47 +0800

    案例完成

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="app">
-    <todo-header></todo-header>
-    <todo-main></todo-main>
-    <todo-footer></todo-footer>
+    <todo-header @addTodoName="addTodo"></todo-header>
+    <todo-main :list="list" @delTodoName="delTodo"></todo-main>
+    <todo-footer :list="list" @clear="clearList"></todo-footer>
   </div>
 </template>
 
@@ -12,11 +12,36 @@ import ToDoMain from './components/ToDoMain.vue';
 import ToDoFooter from './components/ToDoFooter.vue';
 export default {
   name: 'App',
+  data() {
+    return {
+      // list: [{ id: 1, content: "打篮球" }, { id: 2, content: "打羽毛球" }, { id: 3, content: "去游泳" }]
+      list: JSON.parse(localStorage.getItem('list')) || []
+    }
+  },
   components: {
     "todo-header": ToDoHeader,
     "todo-main": ToDoMain,
     "todo-footer": ToDoFooter,
-  }
+  },
+  methods: {
+    addTodo(name) {
+      this.list.unshift({ id: this.list.length + 1, content: name });
+    },
+    delTodo(id) {
+      this.list = this.list.filter(item => item.id !== id);
+    },
+    clearList() {
+      this.list = [];
+    }
+  },
+  watch: {
+    list: {
+      deep: true,
+      handler() {
+        localStorage.setItem('list', JSON.stringify(this.list))
+      }
+    }
+  },
 }
 </script>
 
